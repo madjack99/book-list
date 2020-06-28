d6be6eafd40d9dd9530db05987507f77e3666543
Alex <kavanokami@gmail.com>
2020-06-28 18:35:43 +0400

    feat: connect to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://alex:test1234@ninja-slb1e.mongodb.net/ninja?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
