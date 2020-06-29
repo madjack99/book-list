724b404f4959fb66f6c2aa95cddc26a8502b082b
Alex <kavanokami@gmail.com>
2020-06-29 18:15:59 +0400

    feat: start AddBook comp

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -15,6 +16,7 @@ function App() {
       <div id='main'>
         <h1>Reading List</h1>
         <BookList />
+        <AddBook />
       </div>
     </ApolloProvider>
   );
---
+++ client/src/components/AddBook.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+const AddBook = () => {
+  const { loading, error, data } = useQuery(getAuthorsQuery);
+
+  const displayAuthors = () => {
+    if (loading) {
+      return <option disabled>Loading Authors</option>;
+    } else {
+      return data.authors.map((author) => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
+      });
+    }
+  };
+
+  return (
+    <form id='add-book'>
+      <div className='field'>
+        <label>Book name:</label>
+        <input type='text' />
+      </div>
+      <div className='field'>
+        <label>Genre:</label>
+        <input type='text' />
+      </div>
+      <div className='field'>
+        <label>Author:</label>
+        <select name='' id=''>
+          <option>Select author</option>
+          {displayAuthors()}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+};
+
+export default AddBook;
